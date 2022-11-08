d00a0ac00d9852bb62b2a6a09128f3357481bbe1
Bao Nguyen <baonlq.97@gmail.com>
2022-11-08 21:38:49 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+var solution = new Solution();
+
+Console.WriteLine(solution.TopKFrequent(new int[] {3,0,1,0}, 1));
+
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) {
+        
+        var dict = new Dictionary<int, int>();
+        var array = new List<int>();
+        var value = 0;
+        for (int i = 0; i < nums.Length; i++) {
+
+            if (!dict.ContainsKey(nums[i])){
+                dict.Add(nums[i], 0);
+            }
+
+            dict[nums[i]] += 1;
+        }
+        
+        dict = dict.OrderByDescending(p => p.Value).ToDictionary(p => p.Key, p => p.Value);
+
+        foreach(var val in dict) {
+            value += 1;
+
+            if (value > k) {
+                break;
+            }
+
+            array.Add(val.Key);
+        }
+
+        return array.ToArray();
+    }
+}
